6084d9b2baf2d5ec32f88ce7fdf05786c681c67f
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-01-23 20:40:14 +0200

    done

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,29 +8,29 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int k = 0;
-    int temp;
-    int min;
+	size_t i, j;
+	int k = 0;
+	int temp;
+	int min;
 
-    for (i = 0; i < size; i++)
-    {
-        min = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[min] > array[j])
-            {
-                min = j;
-                k = 1;
-            }
-        }
-        if (k == 1)
-        {
-            temp = array[i];
-            array[i] = array[min];
-            array[min] = temp;
-            print_array(array, size);
-        }
-        k = 0;
-    }
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+			{
+				min = j;
+				k = 1;
+			}
+		}
+		if (k == 1)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+		k = 0;
+	}
 }
