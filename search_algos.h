1d1053b08c85ebe7cb64bbca964b0e27c2570534
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-05-11 20:47:59 +0300

    done

---
+++ search_algos.h
@@ -0,0 +1,7 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* Function prototype for interpolation search */
+int interpolation_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
